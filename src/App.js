8fa675abb85070ad59ae9ca569cd69e29067a6f1
Nidnidh <Bhattnidhi400@gmail.com>
2024-09-27 17:21:12 +0530

    Initial commit with project files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// src/App.js
+
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -1,4 +1,4 @@
-// src/pages/AddUser.js
+
 import React, { useState } from 'react';
 
 function AddUser() {
